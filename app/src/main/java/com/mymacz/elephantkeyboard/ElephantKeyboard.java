4a2af8c046cca438b4161742ba3a9fa4895cad35
springsama <spring_maprang@hotmail.com>
2015-10-31 19:01:28 +0700

    Keyboard

--- app/src/main/java/com/mymacz/elephantkeyboard/ElephantKeyboard.java
+++ app/src/main/java/com/mymacz/elephantkeyboard/ElephantKeyboard.java
@@ -4,9 +4,13 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.text.method.MetaKeyKeyListener;
+import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.CompletionInfo;
 import android.view.inputmethod.InputConnection;
+import android.widget.Toast;
 
 /**
  * Created by mynameismaxz on 10/30/2015 AD.
@@ -14,19 +18,41 @@ import android.view.inputmethod.InputConnection;
 public class ElephantKeyboard extends InputMethodService
     implements KeyboardView.OnKeyboardActionListener{
 
+    static final boolean PROCESS_HARD_KEYS = true;
+
+    private StringBuilder mComposing = new StringBuilder();
+    private boolean mPredictionOn;
+    private boolean mCompletionOn;
+    private int mLastDisplayWidth;
+    private boolean mCapsLock;
+    private long mLastShiftTime;
+    private long mMetaState;
+
+    static final int KEYCODE_CHANGE_LANGUAGETH = -99;
+    static final int KEYCODE_CHANGE_LANGUAGEEN = -98;
+    static final int KEYCODE_SYMS = -3;
+    static final int KEYCODE_THAI_CAP = -2;
+    static final int KEYCODE_EN_CAP = -1;
+
     // initial keyboard view
     private KeyboardView kv;
-    private Keyboard keyboard;
+    private Keyboard keyboardEN;
+    private Keyboard keyboardTH;
+    private Keyboard keyboardSyms;
+    private Keyboard keyboardTH_CAP;
+    private Keyboard keyboardEN_CAP;
 
-    //initial capslock key
-    private boolean caps = false;
 
     @Override
     public View onCreateInputView() {
         // TODO: When callback activity to using keyboard try to edit this method.
         kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard,null);
-        keyboard = new Keyboard(this,R.xml.qwerty);
-        kv.setKeyboard(keyboard);
+        keyboardEN = new Keyboard(this,R.xml.qwerty);
+        keyboardTH = new Keyboard(this,R.xml.qwertyth);
+        keyboardSyms = new Keyboard(this,R.xml.syms);
+        keyboardTH_CAP = new Keyboard(this,R.xml.qwertyth_shift);
+        keyboardEN_CAP = new Keyboard(this,R.xml.qwerty_shift);
+        kv.setKeyboard(keyboardEN);
         kv.setOnKeyboardActionListener(this);
         kv.invalidateAllKeys();
         return kv;
@@ -41,18 +67,37 @@ public class ElephantKeyboard extends InputMethodService
     public void onRelease(int primaryCode) {
 
     }
-
     @Override
     public void onKey(int primaryCode, int[] keyCodes) {
         InputConnection ic = getCurrentInputConnection();
         playClick(primaryCode);
         switch (primaryCode) {
-            case Keyboard.KEYCODE_DELETE :
+            case KEYCODE_CHANGE_LANGUAGETH:
+                kv.setKeyboard(keyboardTH);
+                kv.setOnKeyboardActionListener(this);
+                kv.invalidateAllKeys();
+                break;
+            case KEYCODE_SYMS:
+                kv.setKeyboard(keyboardSyms);
+                kv.setOnKeyboardActionListener(this);
+                kv.invalidateAllKeys();
+                break;
+            case KEYCODE_CHANGE_LANGUAGEEN:
+                kv.setKeyboard(keyboardEN);
+                kv.setOnKeyboardActionListener(this);
+                kv.invalidateAllKeys();
+                break;
+            case KEYCODE_THAI_CAP:
+                kv.setKeyboard(keyboardTH_CAP);
+                kv.setOnKeyboardActionListener(this);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DELETE:
                 ic.deleteSurroundingText(1,0);
                 break;
-            case Keyboard.KEYCODE_SHIFT :
-                caps = !caps;
-                keyboard.setShifted(caps);
+            case KEYCODE_EN_CAP:
+                kv.setKeyboard(keyboardEN_CAP);
+                kv.setOnKeyboardActionListener(this);
                 kv.invalidateAllKeys();
                 break;
             case Keyboard.KEYCODE_DONE :
@@ -60,9 +105,6 @@ public class ElephantKeyboard extends InputMethodService
                 break;
             default :
                 char code = (char) primaryCode;
-                if(Character.isLetter(code) && caps){
-                    code = Character.toUpperCase(code);
-                }
                 ic.commitText(String.valueOf(code),1);
         }
     }
